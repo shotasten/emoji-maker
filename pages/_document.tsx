5c5ed88a89864b75da1a5da7016cabf5c8f9cd44
shotaste <150376648+shotasten@users.noreply.github.com>
2026-05-14 02:47:09 +0900

    feat: Google Analytics (GA4) 追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,15 @@ export default function Document() {
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#FF2D78" />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FB8FMQ2MF6" />
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-FB8FMQ2MF6');
+        `}} />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
